c472fdb1f3cf609cba06780ea23a7aa903a65d9c
Thomas Roux <troux@cscanada.ca>
2026-04-17 08:22:44 -0400

    re organised + added Lagrange

---
+++ Internal_constants.py
@@ -0,0 +1,158 @@
+# Constantes fondamentales
+
+# Constante gravitationnelle universelle
+G = 6.67430 * 10**(-11)
+
+# Vitesse de la lumière dans le vide		
+c = 299792458299792458
+
+# Masses
+M_Soleil = 1.98847 * (10**30)
+M_Terre	= 5.97219 * (10**24)
+M_Lune = 7.34767309 * (10**22)
+
+# Constantes gravitationnelles
+Mu_Soleil = 1.32712440018 * (10**20)
+Mu_Terre = 3.986004418 * (10**14)
+Mu_Lune = 4.9048695 * (10**12)
+
+# Constantes Mathématiques
+PI = 3.141592653589793
+TWO_PI = 2 *PI
+
+J2 = 1.08263e-3
+Rt = 6378.137  # Rayon terrestre (km)
+Mu_Terre_km = 398600.44 # Paramètre grav. (km^3/s^2)
+
+# ----------------------------------------------------------
+# CONSTANTES PHYSIQUES NORMALISÉES
+# ----------------------------------------------------------
+# Unités : distance en UA, masse en masse solaire, temps en jours
+AU = 1.495978707*(10**11)
+Day = 86400
+G_norm = G * (AU**(-3)) * M_Soleil * (Day**2)
+
+
+# ----------------------------------------------------------
+# CONDITIONS INITIALES DES PLANÈTES (Positions [UA], Vitesses [UA/jour], Masse [M_sol])
+# ----------------------------------------------------------
+
+# Soleil
+M_Soleil_norm   = 1.0
+X0_Soleil_norm  = -1.6132380e-3
+Y0_Soleil_norm  =  2.3674938e-3
+Z0_Soleil_norm  = -3.4999903e-5
+VX0_Soleil_norm =  6.0453208e-6
+VY0_Soleil_norm = -1.8980631e-6
+VZ0_Soleil_norm = -1.3060634e-7
+
+# Mercure
+M_Mercure_norm   = 1.6600e-7
+X0_Mercure_norm  =  3.4942761e-1
+Y0_Mercure_norm  =  1.0619088e-2
+Z0_Mercure_norm  = -3.1182970e-2
+VX0_Mercure_norm = -6.4674481e-3
+VY0_Mercure_norm =  2.9372684e-2
+VZ0_Mercure_norm =  2.9939160e-3
+
+# Vénus
+M_Venus_norm   = 2.4476e-6
+X0_Venus_norm  = -5.7685710e-1
+Y0_Venus_norm  =  4.2639983e-1
+Z0_Venus_norm  =  3.9039256e-2
+VX0_Venus_norm = -1.2159929e-2
+VY0_Venus_norm = -1.6321487e-2
+VZ0_Venus_norm =  4.7839154e-4
+
+# Terre
+M_Terre_norm   = 3.0032e-6
+X0_Terre_norm  =  6.8900355e-1
+Y0_Terre_norm  =  7.0799513e-1
+Z0_Terre_norm  = -5.4762805e-5
+VX0_Terre_norm = -1.2606830e-2
+VY0_Terre_norm =  1.1932472e-2
+VZ0_Terre_norm = -5.3326343e-7
+
+# Mars
+M_Mars_norm   = 3.2268e-7
+X0_Mars_norm  =  4.3534005e-1
+Y0_Mars_norm  = -1.3548686
+Z0_Mars_norm  = -3.9100527e-2
+VX0_Mars_norm =  1.3851438e-2
+VY0_Mars_norm =  5.5002850e-3
+VZ0_Mars_norm = -2.2479471e-4
+
+# Jupiter
+M_Jupiter_norm   = 9.5425e-4
+X0_Jupiter_norm  =  1.8147606
+Y0_Jupiter_norm  =  4.7059241
+Z0_Jupiter_norm  = -6.0234633e-2
+VX0_Jupiter_norm = -7.1329432e-3
+VY0_Jupiter_norm =  3.0767709e-3
+VZ0_Jupiter_norm =  1.4683761e-4
+
+# Saturne
+M_Saturne_norm   = 2.8572e-4
+X0_Saturne_norm  = -8.2319838
+Y0_Saturne_norm  = -5.2651142
+Z0_Saturne_norm  =  4.1915711e-1
+VX0_Saturne_norm =  2.7027945e-3
+VY0_Saturne_norm = -4.7129500e-3
+VZ0_Saturne_norm = -2.5483556e-5
+
+# Uranus
+M_Uranus_norm   = 4.3643e-5
+X0_Uranus_norm  =  1.9916762e+01
+Y0_Uranus_norm  =  2.3700466
+Z0_Uranus_norm  = -2.4922728e-1
+VX0_Uranus_norm = -4.9354675e-4
+VY0_Uranus_norm =  3.7222065e-3
+VZ0_Uranus_norm =  2.0227673e-5
+
+# Neptune
+M_Neptune_norm   = 5.1486e-5
+X0_Neptune_norm  =  2.6481558e+01
+Y0_Neptune_norm  = -1.4076556e+01
+Z0_Neptune_norm  = -3.2040642e-1
+VX0_Neptune_norm =  1.4527105e-3
+VY0_Neptune_norm =  2.7901455e-3
+VZ0_Neptune_norm = -9.1342324e-5
+
+# Pluton
+M_Pluton_norm   = 6.6060e-9
+X0_Pluton_norm  =  4.9371124
+Y0_Pluton_norm  = -3.1890424e+01
+Z0_Pluton_norm  =  1.9843679
+VX0_Pluton_norm =  3.1711953e-3
+VY0_Pluton_norm = -1.4100225e-4
+VZ0_Pluton_norm = -9.0807325e-4
+
+# Lune (The Moon)
+# --- LUNE (Basée sur tes positions Terre) ---
+M_Lune_norm   = 3.6942e-8      
+
+# Position : On décale la Lune sur l'axe X par rapport à la Terre (+ 0.00257 UA)
+X0_Lune_norm  = 6.8900355e-1 + 0.00257  
+Y0_Lune_norm  = 7.0799513e-1
+Z0_Lune_norm  = -5.4762805e-5
+
+# Vitesse : On prend la vitesse de la Terre et on ajoute la vitesse orbitale 
+# de la Lune sur l'axe Y pour qu'elle soit perpendiculaire au décalage X.
+VX0_Lune_norm = -1.2606830e-2
+VY0_Lune_norm = 1.1932472e-2 + 0.00059  
+VZ0_Lune_norm = -5.3326343e-7
+
+# --- VISUALISATION ---
+# --- ÉLÉMENTS KÉPLÉRIENS COMPLETS (Moyennes approximatives) ---
+# [a, e, omega (en degrés)]
+KEPLER_ELEMENTS = {
+    "Mercure": [0.3871, 0.2056, 77.4],
+    "Vénus":   [0.7233, 0.0067, 131.5],
+    "Terre":   [1.0000, 0.0167, 102.9],
+    "Mars":    [1.5237, 0.0934, 336.0],
+    "Jupiter": [5.2034, 0.0484, 14.7],
+    "Saturne": [9.5371, 0.0541, 92.4],
+    "Uranus":  [19.191, 0.0472, 170.9],
+    "Neptune": [30.069, 0.0086, 44.9],
+    "Pluton":  [39.482, 0.2488, 224.0]
+}
---
+++ Internal_functions.py
@@ -0,0 +1,246 @@
+
+import numpy as np
+from Internal_constants import *
+
+# --- FONCTIONS DE SIMULATION ---
+class Satellite:
+
+    def __init__(self,a,e,i,OMEGA,w,M):
+        self.a = a
+        self.e = e
+        self.i = i
+        self.OMEGA = OMEGA
+        self.w = w
+        self.M = M
+
+
+    def get_state_vector(self):
+        """Retourne le vecteur d'état [a,e,i,OMEGA,w,M]"""
+        return np.array([self.a, self.e, self.i, self.OMEGA, self.w, self.M])
+
+    def set_state_vector(self, state_vector):
+        """Met à jour les éléments orbitaux à partir du vecteur d'état"""
+        self.a, self.e, self.i, self.OMEGA, self.w, self.M = state_vector
+
+
+def get_keplerian_acceleration(pos, M_central, G):
+    """
+    Calcul pur de l'accélération gravitationnelle vectorielle.
+    Formule : a = - (G * M * pos) / |pos|^3
+    """
+    r_mag = np.linalg.norm(pos)
+    # Protection contre la division par zéro
+    if r_mag == 0: return np.zeros(3) 
+    return - (G * M_central * pos) / r_mag**3
+
+def compute_accel_nBody(R, Masses, nBody):
+    # 1. Extraction et remise en forme (Phase Space)
+    pos = R[:3*nBody].reshape((nBody, 3))
+    vel = R[3*nBody:].reshape((nBody, 3))
+    accel = np.zeros((nBody, 3))
+    
+    # 2. Somme des interactions par paires
+    for i in range(nBody):
+        for j in range(i + 1, nBody):
+            # Vecteur de i vers j
+            r_ij = pos[j] - pos[i]
+            
+            # Accélération de i subie par j (vers j)
+            # On inverse le signe car get_keplerian renvoie un vecteur vers l'origine
+            a_i_par_j = -get_keplerian_acceleration(r_ij, Masses[j], G_norm)
+            
+            # Accélération de j subie par i (vers i)
+            a_j_par_i = -get_keplerian_acceleration(-r_ij, Masses[i], G_norm)
+            
+            accel[i] += a_i_par_j
+            accel[j] += a_j_par_i
+            
+    # 3. Recombinaison du vecteur d'état dérivé
+    return np.concatenate([vel.flatten(), accel.flatten()])
+
+
+# --- MOTEURS RK4 ---
+
+def rk4_step_nBody(R, Masses, nBody, h):
+    """Pas RK4 pour le système complet (N corps)"""
+    k1 = h * compute_accel_nBody(R, Masses, nBody)
+    k2 = h * compute_accel_nBody(R + 0.5 * k1, Masses, nBody)
+    k3 = h * compute_accel_nBody(R + 0.5 * k2, Masses, nBody)
+    k4 = h * compute_accel_nBody(R + k3, Masses, nBody)
+    
+    return R + (k1 + 2*k2 + 2*k3 + k4) / 6
+
+# --- PHYSIQUE : DÉRIVÉE POUR 2 CORPS (SOLEIL FIXE) ---
+def compute_accel_sun_norm(R, t):
+    """
+    R : [x, y, z, vx, vy, vz]
+    Utilise la brique de base get_keplerian_acceleration
+    """
+    pos = R[:3]
+    vel = R[3:]
+    
+    # Appel de la fonction atomique
+    accel = get_keplerian_acceleration(pos, M_Soleil_norm, G_norm)
+    
+    return np.concatenate([vel, accel])
+
+
+# --- 1. MODÈLE PHYSIQUE : ÉQUATIONS DE LAGRANGE (J2) ---
+def lagrange_j2_derivatives(R, t):
+    """
+    Calcule les dérivées temporelles des éléments orbitaux [a, e, i, OMEGA, w, M].
+    Seuls OMEGA, w et M ont des dérivées non nulles dues à J2.
+    """
+    a, e, i, OMEGA, w, M = R
+
+    n = np.sqrt(Mu_Terre_km / a**3) # Moyen mouvement
+
+    # Pas de variations séculaires pour a, e, i au 1er ordre
+    dadt = 0
+    dedt = 0
+    didt = 0
+    
+    # Précession du nœud ascendant (Rotation du plan orbital)
+    dOMEGAdt = - (3 * n * J2 * (Rt**2) * np.cos(i)) / (2 * a**2 * (1 - e**2)**2)
+    
+    # Rotation de l'argument du périgée (Rotation de l'ellipse dans son plan)
+    dwdt = (3 * n * J2 * Rt**2 * (5 * np.cos(i)**2 - 1)) / (4 * a**2 * (1 - e**2)**2)
+    
+    # Évolution de l'anomalie moyenne (Vitesse orbitale + correction J2)
+    dMdt = n + (3 * n * J2 * Rt**2 * (3 * np.cos(i)**2 - 1)) / (4 * a**2 * (1 - e**2)**(1.5))
+
+    return np.array([dadt, dedt, didt, dOMEGAdt, dwdt, dMdt])
+
+# --- 2. INTÉGRATEUR : RUNGE-KUTTA 4 ---
+def rk4_step(f, t, y, h):
+    k1 = h * f(y, t)
+    k2 = h * f(y + 0.5 * k1, t + 0.5 * h)
+    k3 = h * f(y + 0.5 * k2, t + 0.5 * h)
+    k4 = h * f(y + k3, t + h)
+    return y + (k1 + 2*k2 + 2*k3 + k4) / 6
+
+# --- 3. CONVERSION : KEPLER -> CARTÉSIEN ---
+def keplerian_elements_to_cartesian(R):
+    """Convertit [a,e,i,Omega,w,M] en vecteur position [x,y,z] ECI."""
+    a, e, i, OMEGA, w, M = R
+
+    # Résolution de l'équation de Kepler (Newton-Raphson)
+    E = M
+    for _ in range(10): 
+        E = E - (E - e * np.sin(E) - M) / (1 - e * np.cos(E))
+
+    # Anomalie vraie
+    v = 2 * np.arctan(np.sqrt((1 + e) / (1 - e)) * np.tan(E / 2))
+
+    # Coordonnées dans le plan orbital
+    r = a * (1 - e**2) / (1 + e * np.cos(v))
+
+    # vérifie que ton vecteur orbital est bien :
+    x_orb = r * np.cos(v)
+    y_orb = r * np.sin(v)
+    z_orb = 0
+    
+    # Matrice de rotation Plan -> ECI
+    co, so = np.cos(OMEGA), np.sin(OMEGA)
+    ci, si = np.cos(i), np.sin(i)
+    cw, sw = np.cos(w), np.sin(w)
+    R_mat = np.array([
+        [co*cw - so*sw*ci, -co*sw - so*cw*ci,  so*si],
+        [so*cw + co*sw*ci, -so*sw + co*cw*ci, -co*si],
+        [sw*si,            cw*si,             ci]
+    ])
+
+
+    # Calcul direct (plus robuste que la matrice array) :
+    X = x_orb * (co*cw - so*sw*ci) - y_orb * (co*sw + so*cw*ci)
+    Y = x_orb * (so*cw + co*sw*ci) - y_orb * (so*sw - co*cw*ci)
+    Z = x_orb * (sw*si)            + y_orb * (cw*si)
+    
+    return np.array([X, Y, Z])
+
+
+
+
+# Protoypes pour dormand_prince, runge_kutta et euler:
+# y: Vecteur d'état. (éléments Képlérien, x,y,z, )
+# t: Epoque actuelle
+# h: Pas d'intégration
+# f: Fonction dans laquelle il y a les calculs pour intégrer les dérivées des variables d'état
+
+"""
+def dormand_prince(y, t, h, f):
+    
+    k1= h * f(t,y)
+    k2= h * f(t+h/5, y + k1/5 )
+    k3= h * f(t+3/10*h, y + 3/40*k1+ 9/40*k2)
+    k4= h * f(t+4/5*h , y +44/45*k1 - 56/15*k2 + 32/9*k3)
+    k5= h * f(t+8/9*h , y + 19372/6561*k1 - 25360/2187*k2 + 64448/6561*k3 - 212/729*k4)
+    k6= h * f(t+h , y + 9017/3168*k1 - 355/33*k2 - 46732/5247*k3 + 49/176*k4 - 5103/18656*k5)
+    k7= h * f(t+h , y + 35/384*k1 + 500/1113*k3 + 125/192*k4 - 2187/6784*k5 + 11/84*k6)
+    
+    y= y + 35/384*k1 + 500/1113*k3 + 125/192*k4 - 2187/6784*k5 + 11/84*k6
+    return y
+
+def runge_kutta(y, t, h, f):
+    
+    l1=h*f(t,y)
+    l2=h*f(t+h/2 , y+ l1/2)
+    l3=h*f(t+h/2, y + l2/2)
+    l4=h*f(t+h,y+l3)
+    
+    y=y + l1/6 + 2/6*l2 +2/6*l3 + l4/6
+    return y
+   
+def euler(y, t, h, f):
+    y=y + h * f(t,y)
+    return y
+
+
+def dR(R,t):
+
+    # Elements orbitaux
+
+    a       = R[0]   
+    e       = R[1]        
+    i       = R[2]    
+    OMEGA   = R[3]    
+    w       = R[4]    
+    M       = R[5]
+    
+    # expression de n
+    
+    n = 2 * np.pi
+    
+    # Derivées partielles du potentiel R qui s'arrête à J2
+    
+    dRda = -(9/2)*((n**2 * rt**2 * J2)/(a*(1-e**2)**(3/2))) * (1/3 - (np.sin(i)**2)/2)
+    dRde = (9/2)*((n**2 * rt**2 * J2 * e)/((1-e**2)**(5/2))) * (1/3 - (np.sin(i)**2)/2)  
+    dRdi= - (3/2)* ( n**2 * rt**2 * J2)/((1-e**2)**(3/2)) * np.cos(i)*np.sin(i)
+    
+    dRdw=0
+    dRdOMEGA=0
+    dRdM=0
+    
+    # système d'equations differentielles de Lagrange: da/dt, de/dt, di/dt, dOMEGA/dt, dw/dt et dM/dt respectivement
+   
+    eq1= (2/(n*a)) * dRdM    
+    eq2= - ((1-e**2)*0.5)*dRdw/(n*e*a**2 ) + (1-e**2)*dRdM / (n*e*a**2)    
+    eq3= - dRdOMEGA / (n*np.sin(i)*(a**2)*(1-e**2)**0.5) + np.cos(i)*dRdw/(n*np.sin(i)*(a**2)*(1-e**2)**0.5)   
+    eq4= dRdi/(n*(a**2)*np.sin(i)*(1-e**2)**0.5)  
+    eq5= ((1-e**2)**0.5)*dRde/(n*e*a**2) - np.cos(i)*dRdi/(n*(a**2)*np.sin(i)*(1-e**2)**0.5)
+    eq6= n - 2*dRda/(n*a) - (1-e**2)*dRde/ (n*e*a**2)
+    
+    
+    return np.array([eq1, eq2, eq3, eq4, eq5, eq6])
+
+
+def rungekutta(  R,  t,  h):
+
+    k1= h*dR( R , t )
+    k2= h*dR( R+0.5*k1 , t+0.5*h )
+    k3= h*dR( R+0.5*k2 , t+0.5*h )
+    k4= h*dR( R+k3 , t+h )
+
+    R= R + (1/6)*( k1+ 2*k2 + 2*k3 + k4 )
+    return R
+  """
